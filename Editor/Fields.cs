3a148c2ce788ade7b0d50333cc9f7dd88156d247
Michał Osiński <michal.osinski101@gmail.com>
2024-10-07 14:04:45 +0200

    added static function for DefaultInspector

--- Editor/Fields.cs
+++ Editor/Fields.cs
@@ -1,6 +1,7 @@
 using System;
 using JetBrains.Annotations;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace UI.Li.Editor
 {
@@ -8,5 +9,7 @@ namespace UI.Li.Editor
     {
         public static IComponent Property(SerializedProperty property, Action<object> onValueChanged = null, string label = null) =>
             PropertyField.V(property, onValueChanged, label);
+        public static IComponent Inspector([NotNull] UnityEditor.Editor editor, params IManipulator[] manipulators) =>
+            DefaultInspector.V(editor, manipulators);
     }
 }
